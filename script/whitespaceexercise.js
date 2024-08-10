c17ae61d0b804d82b32942ce733533918dc5e067
YOUNG-PAL <ayanyemiopeyemi70@gmail.com>
2024-08-10 13:37:35 +0100

    whitespaceexercise

---
+++ script/whitespaceexercise.js
@@ -0,0 +1,39 @@
+// Convert this text into uppercase: Software Developer
+// let softwaredevelop;
+// softwaredevelop = window.prompt("which software");
+// softwaredevelop= softwaredevelop.toUpperCase();
+
+// console.log(softwaredevelop);
+
+
+// Convert the text Osun State into lowercase:
+// let text = "Osun State";
+// text = window.prompt("text");
+// console.log(text.toLowerCase());
+// // Output: osun state
+
+//Use the slice method to console.log only apple out of this statement: I love to eat apple
+// let text = "I love to eat apple";
+// text = window.prompt("text");
+// console.log(text.slice(14, 19));
+// // Output: apple
+
+// Write a JavaScript program that prints your full name to the console using a text literal
+// console.log("Hammed Ayanyemi");
+
+
+// Create two separate text literals, one for your first name and one for your last name. Combine them into a single text literal and print it to the console.
+// let firstName = "Hammed";
+// let lastName = "Ayanyemi";
+// let fullName = firstName + " " + lastName;
+// console.log(fullName);
+// // Output: Hammed Ayanyemi
+
+// let firstName = "Hammed";
+// let lastName = "Ayanyemi";
+// let hobby = "coding";
+// let introduction = `Hello, my name is ${firstName} ${lastName} and I enjoy ${hobby}.`;
+// document.getElementById("introduction").innerText = introduction;
+
+
+
