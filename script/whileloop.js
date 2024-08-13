94f1c81c5168dce6490387c26efc36941332f571
YOUNG-PAL <ayanyemiopeyemi70@gmail.com>
2024-08-13 22:07:50 +0100

    whileloop

---
+++ script/whileloop.js
@@ -0,0 +1,59 @@
+// let username = "";
+// while (username === "" || username === null) {
+//     username = window.prompt("Enter Username");
+// }
+
+// console.log(username);
+
+// do {
+//     username = window.prompt("Enter Username")
+// } while (username === "" || username === null);
+
+// console.log(username);
+
+
+let loggedIn = false;
+let username;
+let password;
+
+// while (!loggedIn) {
+//     username = window.prompt("Enter your Username");
+//     password = window.prompt("Enter your password");
+
+//     if (username === "olumide" && password === "myPassword") {
+//         loggedIn = true;
+//         console.log("you have logged in successfully");
+//     } else {
+//         console.log("invalid login details");
+//     }
+// }
+
+// do {
+//     username = window.prompt("Enter your Username");
+//     password = window.prompt("Enter your password");
+
+//     if (username === "olumide" && password === "myPassword") {
+//         loggedIn = true;
+//         console.log("you have logged in successfully");
+//     } else {
+//         console.log("invalid login details");
+//     }
+// } while (!loggedIn);
+
+// for (let i = 4; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 20; i > 1; i--) {
+//     console.log(i);
+// }
+
+
+for (let i = 1; i < 20; i++) {
+    if (i == 9) {
+        continue;
+        //break; *if you dont want it to continue*
+    } else {
+        console.log(i);
+    }
+}
---
+++ script/whileloopexercise.js
@@ -0,0 +1,76 @@
+//  Print the numbers from 1 to 10 in the console.
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+
+// Calculate the sum of all numbers from 1 to 100 and print the result.
+// let sum = 0;
+// for (let i = 1; i <= 100; i++) {
+//     sum += i;
+// }
+// console.log("Sum:", sum);
+
+
+//Print all the even numbers between 1 and 20
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+// Print the numbers from 10 down to 1.
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i);
+// }
+
+// This loop prints the multiplication table of 5
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`5 x ${i} = ${5 * i}`);
+// }
+
+// Loop Through an Array
+// const fruits = ["apple", "banana", "orange", "grape"];
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
+
+
+// This loop calculates the factorial of a given number
+// let n = 5; // You can change this value to calculate the factorial of another number
+// let factorial = 1;
+// for (let i = n; i > 1; i--) {
+//     factorial *= i;
+// }
+// console.log(`Factorial of ${n} is:`, factorial);
+
+
+// Sum of Array Elements
+// const numbers = [5, 10, 15, 20, 25];
+// let totalSum = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//     totalSum += numbers[i];
+// }
+// console.log("Total Sum:", totalSum);
+
+
+// This loop reverses a string
+// let str = "space"; // You can change this string to reverse another string
+// let reversedStr = "";
+// for (let i = str.length - 1; i >= 0; i--) {
+//     reversedStr += str[i];
+// }
+// console.log("Reversed String:", reversedStr);
+
+
+// This loop counts the number of vowels in a given string
+// let inputString = "Barcelona"; // You can change this string to count vowels in another string
+// let vowelCount = 0;
+// const vowels = "aeiouAEIOU";
+
+// for (let i = 0; i < inputString.length; i++) {
+//     if (vowels.includes(inputString[i])) {
+//         vowelCount++;
+//     }
+// }
+// console.log("Number of Vowels:", vowelCount);
